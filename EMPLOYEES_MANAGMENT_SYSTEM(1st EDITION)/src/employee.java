76a86dd68a855d0c8c1b7e7e6cefc6765542a311
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2020-12-28 16:24:40 +0200

    Add files via upload

---
+++ EMPLOYEES_MANAGMENT_SYSTEM(1st EDITION)/src/employee.java
@@ -0,0 +1,14 @@
+
+public class employee {
+   public String name;
+   public String depid;
+   public double salary;
+   public String cv;
+   public employee(String n,String d,double s,String c)
+   {
+	   this.name=n;
+	   this.depid=d;
+	   this.salary=s;
+	   this.cv=c;
+   }
+}
